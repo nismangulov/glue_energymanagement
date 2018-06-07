d84d426d6d679f046c6c425507c55be72ae6a6c0
Nail Ismangulov <n.ismangulov@gmail.com>
2018-06-07 11:12:55 +0600

    rework dashboard layout

--- src/dashboard.vue
+++ src/dashboard.vue
@@ -1,12 +1,8 @@
 <template>
-
-<v-container grid-list-md fill-height>
-   <v-layout column>
-      <v-flex d-flex md4>
-         <v-flex d-flex md12>
-            <v-layout row wrap>
-
-
+   <v-container grid-list-md fill-height>
+      <v-layout column>
+         <v-flex d-flex md4>
+            <v-layout class="fix-layout" row wrap>
                <v-flex d-flex md3>
                   <v-card color="red" dark>
                      <v-card-title primary class="title">Waste</v-card-title>
@@ -19,14 +15,12 @@
                      <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
                   </v-card>
                </v-flex>
-
                <v-flex d-flex md3>
                   <v-card color="purple" dark>
                      <v-card-title primary class="title">Smart lighting</v-card-title>
                      <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
                   </v-card>
                </v-flex>
-
                <v-flex d-flex md3>
                   <v-card color="indigo" dark>
                      <v-card-title primary class="title">Video</v-card-title>
@@ -35,16 +29,13 @@
                </v-flex>
             </v-layout>
          </v-flex>
-      </v-flex>
-
-      <v-flex d-flex md8>
-         <v-flex d-flex md6>
-
+         <v-flex d-flex md8>
+            <v-layout row wrap>
+               <v-flex d-flex md6>
                   <v-layout row wrap>
                      <v-flex d-flex md12>
                         <v-card color="blue" dark>
                            <v-card-title primary class="title">Map</v-card-title>
-
                         </v-card>
                      </v-flex>
                   </v-layout>
@@ -53,51 +44,50 @@
                   <v-layout column>
                      <v-flex d-flex md6>
                         <v-layout row wrap>
-                              <v-flex d-flex md12>
-
+                           <v-flex class="fix-layout" d-flex md12>
+                              <v-layout class="fix-layout" row wrap>
                                  <v-flex d-flex md6>
                                     <v-card color="green" dark>
                                        <v-card-title primary class="title">Meters</v-card-title>
                                        <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
                                     </v-card>
                                  </v-flex>
-
                                  <v-flex d-flex md6>
-                                    <v-card color="orange" dark>
-                                       <v-card-title primary class="title">Air</v-card-title>
-                                       <v-card-text>{{ lorem.slice(0, 50) }}</v-card-text>
-                                    </v-card>
-                                    <v-card color="lime" dark>
-                                       <v-card-title primary class="title">Weather</v-card-title>
-                                       <v-card-text>{{ lorem.slice(0, 50) }}</v-card-text>
-                                    </v-card>
+                                    <v-layout class="fix-layout" row wrap>
+                                       <v-flex d-flex md6>
+                                          <v-card color="orange" dark>
+                                             <v-card-title primary class="title">Air</v-card-title>
+                                             <v-card-text>{{ lorem.slice(0, 45) }}</v-card-text>
+                                          </v-card>
+                                       </v-flex>
+                                       <v-flex d-flex md6>
+                                          <v-card color="lime" dark>
+                                             <v-card-title primary class="title">Weather</v-card-title>
+                                             <v-card-text>{{ lorem.slice(0, 45) }}</v-card-text>
+                                          </v-card>
+                                       </v-flex>
+                                    </v-layout>
                                  </v-flex>
-
-                              </v-flex>
+                              </v-layout>
+                           </v-flex>
                         </v-layout>
                      </v-flex>
-                     <v-flex d-flex md6>
-                        <v-layout row wrap>
-                              <v-flex d-flex md12>
-
-                                 <v-flex d-flex md12>
-                                    <v-card color="teal" dark>
-                                       <v-card-title primary class="title">Power</v-card-title>
-                                       <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
-                                    </v-card>
-                                 </v-flex>
-
-                              </v-flex>
+                     <v-flex class="move-top" d-flex md6>
+                        <v-layout class="fix-layout-large" row wrap>
+                           <v-flex d-flex md12>
+                              <v-card color="teal" dark>
+                                 <v-card-title primary class="title">Power</v-card-title>
+                                 <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
+                              </v-card>
+                           </v-flex>
                         </v-layout>
                      </v-flex>
                   </v-layout>
-
+               </v-flex>
+            </v-layout>      
          </v-flex>
-      </v-flex>
-   </v-layout>
-</v-container>
-
-
+      </v-layout>
+   </v-container>
 </template>
 
 <script>
@@ -115,4 +105,16 @@ export default {
 };
 </script>
 
+<style>
+   .container.fill-height .layout.fix-layout {
+      height: calc(100% + 8px)
+   }
 
+   .container.fill-height .layout.fix-layout-large {
+      height: calc(100% + 16px)
+   }
+
+   .move-top {
+      margin-top: -8px;
+   }
+</style>
