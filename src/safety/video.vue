5de0e2a26a078bfae29380ec90fb3c6443060fa2
vvzvlad <vvzvlad@gmail.com>
2018-06-15 16:34:25 +0300

    add video

--- src/safety/video.vue
+++ src/safety/video.vue
@@ -1,15 +1,32 @@
 <template>
-   <v-container fill-height>
-      <v-layout row wrap>
-         <v-flex d-flex md12>
-            <v-card color="green" dark>
-               <v-card-title primary class="title">{{$route.path}}
-               </v-card-title>
-               <v-card-text>
-               </v-card-text>
-            </v-card>
+   <v-container grid-list-md fill-height>
+      <v-layout column>
+         <v-flex d-flex md6>
+            <v-layout row wrap fill-height fix-layout>
+               <v-flex d-flex md6 fix-layout>
+                  <v-card color="green" dark>
+                  </v-card>
+               </v-flex>
+               <v-flex d-flex md6>
+                  <v-card color="green" dark>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-flex>
+         <v-flex d-flex md6>
+            <v-layout row wrap fill-height fix-layout>
+               <v-flex d-flex md6 fix-layout>
+                  <v-card color="green" dark>
+                  </v-card>
+               </v-flex>
+               <v-flex d-flex md6>
+                  <v-card color="green" dark>
+                  </v-card>
+               </v-flex>
+            </v-layout>
          </v-flex>
       </v-layout>
+
    </v-container>
 </template>
 
@@ -29,6 +46,10 @@ export default {
 <style>
 .fill-height {
   height: 100%;
+}
+
+.container.fill-height .layout.fix-layout {
+  height: calc(100% + 8px);
 }
 </style>
 
