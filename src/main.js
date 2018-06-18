ef9ef79a582a60c37ea5bcfe702f65ecae241f30
Nail Ismangulov <n.ismangulov@gmail.com>
2018-06-18 09:23:03 +0600

    fixed leaflet icons

--- src/main.js
+++ src/main.js
@@ -143,6 +143,15 @@ const router = new VueRouter({
    }]
 })
 
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('../node_modules/leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('../node_modules/leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('../node_modules/leaflet/dist/images/marker-shadow.png')
+})
 
 new Vue({
    el: '#app',
