1b1dd19544e5737471596cc2a30a6ec965d53d09
vvzvlad <vvzvlad@gmail.com>
2018-06-22 18:23:46 +0300

    fix bugs, refactoring

--- src/waste/components/bindetails.vue
+++ src/waste/components/bindetails.vue
@@ -1,29 +1,26 @@
 <template>
    <div>
-      <v-dialog v-model="visible" max-width="300px" overlay-hide style="z-index: 1 ">
+      <v-dialog v-model="visible" max-width="300px" overlay-hide style="z-index: 1">
          <v-card>
             <v-card-title>
-               <div class="title text-xs-center ">{{props.name}} : {{props.status}} </div>
+               <div class="title text-xs-center">{{values.name}}: {{values.status}}</div>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-               <div class="subheading ">Utilization level:</div>
-               {{props.level}}
-               <v-progress-circular v-model="props.level" size="80" width="40" color="secondary"></v-progress-circular>
+               <div class="subheading">Utilization level: {{values.level}}%</div>
+               <v-progress-linear v-model="values.level" height="20" color="secondary"></v-progress-linear>
             </v-card-text>
             <v-divider></v-divider>
             <v-card-text>
-               <div class="subheading ">Battery status:</div>
-               {{props.battery}}
-               <v-progress-linear v-model="props.battery" height="20" color="secondary"></v-progress-linear>
+               <div class="subheading">Battery level: {{values.battery}}%</div>
+               <v-progress-linear v-model="values.battery" height="20" color="secondary"></v-progress-linear>
             </v-card-text>
             <v-divider></v-divider>
             <v-card-text>
-               <div class="subheading ">Location:</div>
-               Lat:{{props.coordinates.lat}} : Lng:{{props.coordinates.lng}}
+               <div class="subheading ">Location: {{values.coordinates.lat}}, {{values.coordinates.lng}}</div>
             </v-card-text>
             <v-card-actions>
-               <v-btn color="primary " flat @click.stop="visible=false ">Close</v-btn>
+               <v-btn color="primary " flat @click.stop="visible=false">Close</v-btn>
             </v-card-actions>
          </v-card>
       </v-dialog>
@@ -36,22 +33,11 @@ import Vue from "vue";
 export default {
   data: () => ({
     visible: false,
-    props: {
-      battery: "",
-      level: "",
-      btype: "",
-      coordinates: "",
-      name: "",
-      visible: false
-    }
+    values: {}
   }),
   methods: {
     show(item) {
-      this.props.level = item.level;
-      this.props.name = item.name;
-      this.props.type = item.type;
-      this.props.battery = item.battery;
-      this.props.coordinates = item.coordinates;
+      this.values = item;
       this.visible = true;
     }
   }
