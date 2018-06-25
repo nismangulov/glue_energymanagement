4dd8ecc18d0ec5f23337cda62c6f40cab551e633
vvzvlad <vvzvlad@gmail.com>
2018-06-25 17:39:09 +0300

    add energy managment

---
+++ src/infrastructure/components/energy_management_details.vue
@@ -0,0 +1,93 @@
+<template>
+   <div>
+      <v-dialog v-model="visible" max-width="300px" overlay-hide style="z-index: 1">
+         <v-card>
+            <v-card-title>
+               <div class="title text-xs-center">{{values.name}}</div>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+               <div class="subheading">Status: {{values.status}}</div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-text>
+               <div class="subheading">Actions</div>
+               <v-btn color="primary" @click.native="on">
+                  <v-icon left small>fa-plug</v-icon> ON
+               </v-btn>
+               <v-btn color="primary" @click.native="off">
+                  <v-icon left small>fa-power-off</v-icon> OFF
+               </v-btn>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-text>
+               <l-map :zoom="map.zoom" :center="map.center">
+                  <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
+                  <l-marker :lat-lng="map.marker"></l-marker>
+               </l-map>
+            </v-card-text>
+            <v-card-actions>
+               <v-btn color="primary" flat @click.stop="visible=false">Close</v-btn>
+            </v-card-actions>
+         </v-card>
+      </v-dialog>
+   </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, Axios);
+
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data: () => ({
+    visible: false,
+    values: {},
+    map: {
+      zoom: 15,
+      center: {
+        lat: "55.659744",
+        lng: "37.696447"
+      },
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a  href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: {
+        lat: "55.659744",
+        lng: "37.696447"
+      }
+    }
+  }),
+  methods: {
+    show(item) {
+      this.values = item;
+      this.visible = true;
+      this.map.center = item.coordinates;
+      this.map.marker = item.coordinates;
+    },
+    on() {
+      Vue.axios
+        .get("http://localhost:8080/system_actions", {
+          params: {
+            action: this[l].action
+          }
+        })
+        .then(response => {})
+        .catch(error => {});
+    },
+    off() {}
+  }
+};
+</script>
+
+<style>
+@import "../../../node_modules/leaflet/dist/leaflet.css";
+</style>
--- src/infrastructure/energy_management.vue
+++ src/infrastructure/energy_management.vue
@@ -2,28 +2,98 @@
    <v-container fill-height>
       <v-layout row wrap>
          <v-flex d-flex md12>
-            <v-card color="green" dark>
-               <v-card-title primary class="title">{{$route.path}}
+            <v-card color="white">
+               <v-card-title class="py-1 px-1">
+
+                  <v-btn color="primary" @click.native="on">
+                     <v-icon left small>fa-plug</v-icon> ON ALL
+                  </v-btn>
+                  <v-btn color="primary" @click.native="off">
+                     <v-icon left small>fa-power-off</v-icon> OFF ALL
+                  </v-btn>
                </v-card-title>
-               <v-card-text>
-               </v-card-text>
+
+               <v-divider></v-divider>
+               <v-data-table :headers="headers" :items="station_table" :pagination.sync="pagination">
+                  <template slot="items" slot-scope="props">
+
+                     <tr @click="$refs.energy_management_details.show(props.item)">
+                        <td>
+                           <div>{{ props.item.name }}</div>
+                        </td>
+                        <td>
+                           <div>{{ props.item.address }}</div>
+                        </td>
+                        <td>
+                           <div>{{ props.item.type }}</div>
+                        </td>
+                        <td>
+                           <div>{{ props.item.status }}</div>
+                        </td>
+                     </tr>
+                  </template>
+               </v-data-table>
             </v-card>
          </v-flex>
       </v-layout>
+      <energy_management_details ref="energy_management_details"></energy_management_details>
+
    </v-container>
 </template>
 
 
 <script>
 import Vue from "vue";
-import Axios from "axios";
-import VueAxios from "vue-axios";
-Vue.use(VueAxios, Axios);
+import tableData from "!json-loader!./stations.json";
+
+import energy_management_details from "./components/energy_management_details.vue";
+Vue.component("energy_management_details", energy_management_details);
 
 export default {
-  data: () => ({})
+  data: () => ({
+    pagination: { rowsPerPage: 25 },
+
+    station_table: tableData,
+    headers: [
+      {
+        text: "Name",
+        value: "name",
+        align: "left"
+      },
+      {
+        text: "Address",
+        value: "address",
+        sortable: false
+      },
+      {
+        text: "Type",
+        value: "type",
+        align: "center"
+      },
+      {
+        text: "Status",
+        value: "status",
+        sortable: false
+      }
+    ]
+  }),
+  methods: {
+    on() {
+      Vue.axios
+        .get("http://localhost:8080/system_actions", {
+          params: {
+            action: this[l].action
+          }
+        })
+        .then(response => {})
+        .catch(error => {});
+    },
+    off() {}
+  }
 };
 </script>
+
+
 
 
 <style>
