dd917eb5554e7759e01a1a2a766eca9275508794
vvzvlad <vvzvlad@gmail.com>
2018-06-27 15:33:01 +0300

    add module.exports.output.publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -4,98 +4,99 @@ var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'build.js'
-    },
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ],
-            }, {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {}
-                    // other vue-loader options go here
-                }
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]?[hash]'
-                }
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'file-loader',
-                options: {
-                    limit: 10000,
-                    name: '[name].[ext]?[hash]'
-                }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        },
-        extensions: ['*', '.js', '.vue', '.json']
-    },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true,
-        overlay: true
-    },
-    performance: {
-        hints: false
-    },
-    devtool: '#eval-source-map',
-    plugins: [
+   entry: './src/main.js',
+   output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'build.js'
+   },
+   module: {
+      rules: [{
+         test: /\.css$/,
+         use: [
+            'vue-style-loader',
+            'css-loader'
+         ],
+      }, {
+         test: /\.vue$/,
+         loader: 'vue-loader',
+         options: {
+            loaders: {}
+            // other vue-loader options go here
+         }
+      },
+      {
+         test: /\.js$/,
+         loader: 'babel-loader',
+         exclude: /node_modules/
+      },
+      {
+         test: /\.(png|jpg|gif|svg)$/,
+         loader: 'file-loader',
+         options: {
+            name: '[name].[ext]?[hash]'
+         }
+      },
+      {
+         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+         loader: 'file-loader',
+         options: {
+            limit: 10000,
+            name: '[name].[ext]?[hash]'
+         }
+      }
+      ]
+   },
+   resolve: {
+      alias: {
+         'vue$': 'vue/dist/vue.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+   },
+   devServer: {
+      historyApiFallback: true,
+      noInfo: true,
+      overlay: true
+   },
+   performance: {
+      hints: false
+   },
+   devtool: '#eval-source-map',
+   plugins: [
       new HtmlWebpackPlugin({
-        title: 'NOKIA IMPACT Glue',
-        meta: {
+         title: 'NOKIA IMPACT Glue',
+         meta: {
             author: 'vvzvlad by nokia iot lab'
-        },
-        filename: 'index.html',
-        inject: false,
-        template: '!!ejs-loader!src/assets/index.ejs'
+         },
+         filename: 'index.html',
+         inject: false,
+         template: '!!ejs-loader!src/assets/index.ejs'
       }),
-  ]
+   ]
 
 }
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map'
-        // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        }),
-        new FaviconsWebpackPlugin({
-            logo: './src/assets/tape_logo.svg',
-            prefix: '/'
-        })
-    ])
+   module.exports.devtool = '#source-map'
+   module.exports.output.publicPath = 'dist'
+   // http://vue-loader.vuejs.org/en/workflow/production.html
+   module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+         'process.env': {
+            NODE_ENV: '"production"'
+         }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+         sourceMap: true,
+         compress: {
+            warnings: false
+         }
+      }),
+      new webpack.LoaderOptionsPlugin({
+         minimize: true
+      }),
+      new FaviconsWebpackPlugin({
+         logo: './src/assets/tape_logo.svg',
+         prefix: '/'
+      })
+   ])
 }
