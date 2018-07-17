fa94057a756bcd8123040f83503dc7f9f2b6599f
Nail Ismangulov <n.ismangulov@gmail.com>
2018-07-17 15:49:28 +0600

    init local heatmap build

--- src/eco_monitoring/map.vue
+++ src/eco_monitoring/map.vue
@@ -5,7 +5,7 @@
             <v-card>
                <l-map :zoom="map.zoom" :center="map.center">
                   <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
-                  <heatmap :data="heatmapData" :options="heatmapOptions"></heatmap>
+                  <colored-heatmap :data="heatmapData" :options="heatmapOptions"></colored-heatmap>
                   <template v-for="marker in map.markers">
                      <l-marker :lat-lng="marker.coordinates" @click="$router.push({path: '/eco_monitoring/history'})" v-bind:key="marker.name"></l-marker>
                   </template>
@@ -24,7 +24,7 @@ import VueAxios from "vue-axios";
 Vue.use(VueAxios, Axios);
 
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
-import Heatmap from "../common/heatmap";
+import ColoredHeatmap from "../common/colored-heatmap";
 
 import table_data from "!json-loader!./stations.json";
 
@@ -33,7 +33,7 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
-    Heatmap
+    ColoredHeatmap
   },
   mounted() {
     //this.update();
@@ -87,7 +87,16 @@ export default {
     heatmapData: {
       min: 1,
       max: 20,
-      data: []
+      data: [
+        {lat: 55.83423792, lng: 37.64075481, count: 18},
+        {lat: 55.7552443, lng: 37.61348755, count: 28},
+        {lat: 55.68098578, lng: 37.70412475, count: 62},
+        {lat: 55.86790868, lng: 37.51461059, count: 32},
+        {lat: 55.77455929, lng: 37.48027832, count: 54},
+        {lat: 55.69259798, lng: 37.54756958, count: 48},
+        {lat: 55.61511815, lng: 37.61211426, count: 78},
+        {lat: 55.83398936, lng: 37.73296387, count: 43}
+      ]
     }
   })
 };
