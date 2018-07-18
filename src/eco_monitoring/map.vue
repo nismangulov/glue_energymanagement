d8aa2c3f648d21c23e7fecbd1c687fbd4685a119
Nail Ismangulov <n.ismangulov@gmail.com>
2018-07-18 17:05:31 +0600

    updated test data

--- src/eco_monitoring/map.vue
+++ src/eco_monitoring/map.vue
@@ -91,12 +91,12 @@ export default {
       data: [
         {lat: 55.83423792, lng: 37.64075481, radius: 7000, color: '#FF0000'},
         {lat: 55.7552443, lng: 37.61348755, radius: 14000, color: '#00FF00'},
-        {lat: 55.68098578, lng: 37.70412475, radius: 7000, color: '#0000FF'},
-        {lat: 55.86790868, lng: 37.51461059, radius: 7000, color: '#FF00FF'},
+        {lat: 55.68098578, lng: 37.70412475, radius: 7000, color: '#FFFF00'},
+        {lat: 55.86790868, lng: 37.51461059, radius: 7000, color: '#FFFF00'},
         {lat: 55.77455929, lng: 37.48027832, radius: 7000, color: '#FFFF00'},
-        {lat: 55.69259798, lng: 37.54756958, radius: 21000, color: '#FF0000'},
+        {lat: 55.69259798, lng: 37.54756958, radius: 14000, color: '#FF0000'},
         {lat: 55.61511815, lng: 37.61211426, radius: 7000, color: '#00FF00'},
-        {lat: 55.83398936, lng: 37.73296387, radius: 7000, color: '#0000FF'}
+        {lat: 55.83398936, lng: 37.73296387, radius: 7000, color: '#FFFF00'}
       ]
     }
   })
