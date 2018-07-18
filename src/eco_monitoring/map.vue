2144ed5f1c9097d9d5884ace03d2bf935f04bdd0
Nail Ismangulov <n.ismangulov@gmail.com>
2018-07-18 16:05:41 +0600

    update colored map

--- src/eco_monitoring/map.vue
+++ src/eco_monitoring/map.vue
@@ -5,15 +5,7 @@
             <v-card>
                <l-map :zoom="map.zoom" :center="map.center">
                   <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
-                      <radialGradient id="exampleGradient">
-                        <stop offset="10%" stop-color="gold"/>
-                        <stop offset="95%" stop-color="green"/>
-                      </radialGradient>
-
-                  <template v-for="circle in heatmapData.data">
-                     <l-circle :lat-lng="circle" fillColor="url(#exampleGradient)" :radius="circle.radius" v-bind:key="circle.index"></l-circle>
-                  </template>
-                  
+                  <colored-heatmap :data="heatmapData"></colored-heatmap>
                   <template v-for="marker in map.markers">
                      <l-marker :lat-lng="marker.coordinates" @click="$router.push({path: '/eco_monitoring/history'})" v-bind:key="marker.name"></l-marker>
                   </template>
@@ -97,14 +89,14 @@ export default {
       min: 1,
       max: 20,
       data: [
-        {lat: 55.83423792, lng: 37.64075481, radius: 18 * 500},
-        {lat: 55.7552443, lng: 37.61348755, radius: 28 * 500},
-        {lat: 55.68098578, lng: 37.70412475, radius: 62 * 500},
-        {lat: 55.86790868, lng: 37.51461059, radius: 32 * 500},
-        {lat: 55.77455929, lng: 37.48027832, radius: 54 * 500},
-        {lat: 55.69259798, lng: 37.54756958, radius: 48 * 500},
-        {lat: 55.61511815, lng: 37.61211426, radius: 78 * 500},
-        {lat: 55.83398936, lng: 37.73296387, radius: 43 * 500}
+        {lat: 55.83423792, lng: 37.64075481, radius: 7000, color: '#FF0000'},
+        {lat: 55.7552443, lng: 37.61348755, radius: 14000, color: '#00FF00'},
+        {lat: 55.68098578, lng: 37.70412475, radius: 7000, color: '#0000FF'},
+        {lat: 55.86790868, lng: 37.51461059, radius: 7000, color: '#FF00FF'},
+        {lat: 55.77455929, lng: 37.48027832, radius: 7000, color: '#FFFF00'},
+        {lat: 55.69259798, lng: 37.54756958, radius: 21000, color: '#FF0000'},
+        {lat: 55.61511815, lng: 37.61211426, radius: 7000, color: '#00FF00'},
+        {lat: 55.83398936, lng: 37.73296387, radius: 7000, color: '#0000FF'}
       ]
     }
   })
