76b893f792507a87756c6758b5771616afb3c4c6
Nail Ismangulov <dialog93@gmail.com>
2018-08-16 13:03:05 +0600

    update power quality table (#15)

--- src/power_quality/index.vue
+++ src/power_quality/index.vue
@@ -3,16 +3,19 @@
       <v-layout row wrap>
          <v-flex d-flex md12>
             <v-card color="transparent" class="no-shadow">
-               <v-data-table :headers="headers" :items="items">
+               <v-data-table :headers="headers" :items="items" hide-actions>
                   <template slot="items" slot-scope="props">
-                     <td class="text-xs-left">
+                     <td class="text-xs-left" :style="get_row_color(props.item.index)">
                         <div>{{ props.item.parameter }}</div>
                      </td>
-                     <td class="text-xs-left">
+                     <td class="text-xs-left" :style="get_row_color(props.item.index)">
                         <div>{{ props.item.value }}</div>
                      </td>
-                     <td class="text-xs-left">
+                     <td class="text-xs-left" :style="get_row_color(props.item.index)">
                         <div>{{ props.item.index }}</div>
+                     </td>
+                     <td v-if="props.index === 0" class="text-xs-left text-center cell-max" :rowspan="items.length" :style="get_row_color(get_max_value())">
+                        {{ get_max_value() }}
                      </td>
                   </template>
                </v-data-table>
@@ -33,7 +36,8 @@ export default {
       headers: [
          {
             text: "Parameter",
-            value: "parameter"
+            value: "parameter",
+            sorting: false
          },
          {
             text: "Value",
@@ -42,6 +46,9 @@ export default {
          {
             text: "Index",
             value: "index"
+         },
+         {
+            text: "Max"
          }
       ],
       items: [
@@ -71,7 +78,24 @@ export default {
             index: "1"
          },
       ]
-   })
+   }),
+   methods: {
+      get_row_color(value) {
+         console.log('value', value)
+         if (value < 1.1) {
+            return "background-color: #DCE9D6;"
+         } else if (value < 2) {
+            return "background-color: #FDF1D3;"
+         }
+
+         return "background-color: #EDCCCC;"
+      },
+      get_max_value() {
+         let arr = this.items.map(item => item.index)
+
+         return Math.max(...arr)
+      }
+   }
 };
 </script>
 
@@ -82,5 +106,13 @@ export default {
 
    .no-shadow > div {
       box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+   }
+
+   .text-center {
+      text-align: center !important;
+   }
+
+   .cell-max {
+      border-bottom: none;
    }
 </style>
